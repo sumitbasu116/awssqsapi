2bc4045ffad6fa630605e0a0fa43cd97c2e8f28b
Sumit <sumitbasu116@gmail.com>
2025-10-25 22:27:53 +0530

    adding initial java files

---
+++ src/main/java/com/sumit/sqs/AwssqsapiApplication.java
@@ -0,0 +1,13 @@
+package com.sumit.sqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwssqsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwssqsapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sumit/sqs/AwssqsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumit.sqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwssqsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
